f2d323cd36bd2be8be9ec7cd38c8dd4e7407f891
anukaw100 <anukaw100@gmail.com>
2021-03-13 19:40:33 +1300

    Aesthetic upgrades & login option
    Fix header not reaching corners
    Temp fix for middle nav & logo allignment
    Add login button & link
    Add div element to main, to organize cleanly
    Add busy parking lot picture
    Add github link

---
+++ src/homepage/react-home.jsx
@@ -0,0 +1,70 @@
+class Header extends React.Component {
+    render() {
+        return (
+            <header id="headerhome">
+                <a href="home.html"> 
+                    <img class="logo" src="logo.gif"/>
+                </a>
+                <nav>
+                    <ol class="list">
+                        <li><a>State of Parking</a></li>
+                        <li><a>Why A.I?</a></li>
+                        <li><a>Our Product</a></li>
+                        <li id="loginicon"><a href="/loginpage/loginpage.html">Login</a></li>
+                    </ol>
+                </nav>
+           </header>
+        );
+    }
+}
+
+class Info extends React.Component {
+    render() {
+        return (
+            <main id="main-home">
+                <div id="information">
+                <h1>The Dilema of Parking!</h1>
+                <p>Current parking methods are inefficient, they are time consuming,
+                   costly, and stressful.This is Filler text purely for demonstrating scroll, 
+                   This is Filler text purely for demonstrating
+                   Current parking methods are inefficient, they are time consuming,
+                   costly, and stressful. This is Filler text purely for demonstrating scroll, 
+                   This is Filler text purely for demonstrating
+                   Current parking methods are inefficient, they are time consuming,
+                   costly, and stressful. This is Filler text purely for demonstrating scroll, 
+                   This is Filler text purely for demonstrating
+                </p> 
+                <br />
+                <img src="busylot.jpg" />
+                </div>
+            </main>
+        );
+    }
+};
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <ol class="list">
+                    <li><a href="https://github.com/Anukaw100/parking-detection-ml-website/tree/main">GitHub</a></li>
+                    <li>Contact Us</li>
+                </ol>
+            </footer>
+        );
+    }
+}
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Info />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Board />, document.getElementById("home")); 
