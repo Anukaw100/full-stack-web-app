b5114bed7e17451edfd41d3b4ad817d39831a839
anukaw100 <anukaw100@gmail.com>
2021-05-11 15:35:47 +1200

    Improve UI of landing page
    Add fixed header height
    Fix overlapping of card on header and sidemenu, using z-index
    Add two new sections with cards
    Change <div> element to <section> for each section
    Add new background color to the second section
    Add new background images to the new cards
    Modify height of each section to be equal to viewport height
    Add some content adjacent to each card in the section

--- src/home-page/home.jsx
+++ src/home-page/home.jsx
@@ -5,16 +5,56 @@ import "./home.css"
 
 function Info() {
   return (
-    <main className="container">
-      <div className="card">
-        <div className="card-content">
-          <h2 className="card-title" >The Future of Parking</h2>
-          <p className="card-body">This card is going to be truly awesome and show case our amazing
-            skills in CSS.
-          </p>
-          <a href="/product" className="button">Learn More</a>
+    <main>
+      <section>
+        <div className="product-info">
+          <h1>Our Future for Parking</h1>
+          <p>Use A.I to assess a congested parking lot</p>
         </div>
-      </div>
+        <div className="card product-card">
+          <div className="card-content">
+            <h2 className="card-title" >The Future of Parking</h2>
+            <p className="card-body">Try out a demo of our parking solution!
+          </p>
+            <a href="/product" className="button">Learn More</a>
+          </div>
+        </div>
+      </section>
+      <section className="light-section">
+        <div className="card ai-card">
+          <div className="card-content">
+            <h2 className="card-title" >AI to the rescue</h2>
+            <p className="card-body">AI are helping us to solve real world
+            problems.
+          </p>
+            <a href="/why-ai" className="button">Learn More</a>
+          </div>
+        </div>
+        <div className="ai-info">
+          <h1>Machine Learning is changing the world</h1>
+          <p>Artifical Intelligence is getting smarter and better at helping us
+            solve various challenges in our everyday world.
+          </p>
+        </div>
+      </section>
+      <section>
+      <div className="parking-info">
+          <h1>Driving in circles</h1>
+          <p>Parking is becoming more laborious every day as traffic increases.
+            There is no way without human supervision of knowing where an empty
+            parking spot could be.
+          </p>
+        </div>
+        <div className="card parking-card">
+          <div className="card-content">
+            <h2 className="card-title" >Parking is painful</h2>
+            <p className="card-body">Cities are facing major problems with
+            parking infrastructure.
+          </p>
+            <a href="/product" className="button">Learn More</a>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
