c5f08d2511e4fd5518702fadb1fc709e2d8d03e6
ss-raicangu <ssCm181126@outlook.com>
2021-05-30 05:03:45 +1200

    Refactor common-sections CSS styles to BEM syntax
    - Refer to [this BEM page](https://en.bem.info/methodology/naming-
      convention/) for an explanation of the syntax used by PostCSS Linter,
      and [this common mistakes article](https://sparkbox.com/foundry/bem_
      by_example) to avoid these mistakes.
    
    - Add multiple BEM-style classes, where necessary.
    - Move logo out of the primary navigation menu.
    - Add `React.ref` to the element `.header__secondary-nav-menu`, which
    - represents the sidebar menu.
    - Rename variables and functions related to the sidebar menu.
    - Split CSS classes into separate files based on their block name. For
      example, `header.css` contains all the `.header_*` CSS styles.
    - Remove explicit units for zero-measurements. For example, `0px` ->
      `0`.
    - Put CSS comments on separate lines.
    - Move unnecessary CSS styles out of the media queries. These include
      the CSS styles of child elements in the secondary-navigation menu,
      which is hidden in a larger window with `display: none;`.
    - Ran the "Prettier" formatter on the CSS and JSX files.
    - Rewrite secondary-navigation button CSS to make it a text-based
      ordinary button.
    - Fix logo CSS style by converting width measurements to height measure.
    - Add comment component definitions at the top of each CSS file. For
      example, `/** @define header */`. See [the PostCSS BEM linter
      docs](https://github.com/postcss/postcss-bem-linter#define-
      componentsutilities-with-a-comment) for further information.
    
    - [BUG] Secondary navigation list has no spacing between list items.
      Reason: Padding is being applied on anchors to no effect.
    - [BUG] Some CSS styling is incomplete.

--- src/common/common-sections.jsx
+++ src/common/common-sections.jsx
@@ -1,51 +1,100 @@
-import React from "react";
+import React, { useRef } from "react";
 import Logo from "Images/logo.gif";
-import "Common/common-sections.css";
+import "Common/css-common-sections/header.css";
+import "Common/css-common-sections/footer.css";
+import "Common/css-common-sections/logo.css";
 
 export function Header() {
-  const openSideBar = () =>
-    document.getElementById("sidebar-menu").classList.toggle("collapsed");
+  const secondaryNavRef = useRef(null);
+
+  const showSecondaryNav = () =>
+    secondaryNavRef.current.classList.toggle(
+      "header__secondary-nav-menu_collapsed"
+    );
 
   return (
-    <header>
-      <nav className="nav-menu">
+    <header className="header">
+      <nav className="header__primary-nav">
         <a className="logo" href="/">
-          <img src={Logo} alt="Website logo" />
+          <img className="logo__img" src={Logo} alt="Website logo" />
         </a>
-        <ol className="list">
+        <ol className="header__list header__list_type_primary">
           <li>
-            <a href="/state-of-parking/">State of Parking</a>
+            <a
+              className="header__link header__link_type_primary"
+              href="/state-of-parking/"
+            >
+              State of Parking
+            </a>
           </li>
           <li>
-            <a href="/why-ai/">Why A.I?</a>
+            <a
+              className="header__link header__link_type_primary"
+              href="/why-ai/"
+            >
+              Why A.I?
+            </a>
           </li>
           <li>
-            <a href="/product/">Product</a>
+            <a
+              className="header__link header__link_type_primary"
+              href="/product/"
+            >
+              Product
+            </a>
           </li>
         </ol>
-        <span className="login-nav-item">
-          <a id="loginlink" href="/account/">
+        <span className="header__login-link">
+          <a id="loginlink" className="header__link" href="/account/">
             Account
           </a>
         </span>
       </nav>
-      <div className="sidebar">
-        <button type="button" title="Toggle sidebar menu" onClick={openSideBar}>
+      <div className="header__secondary-nav">
+        <button
+          type="button"
+          className="header__secondary-nav-button"
+          title="Toggle visibility of sidebar"
+          onClick={showSecondaryNav}
+        >
           Menu
         </button>
-        <nav id="sidebar-menu" className="collapsed">
-          <ol className="list">
+        <nav
+          ref={secondaryNavRef}
+          className="header__secondary-nav-menu header__secondary-nav-menu_collapsed"
+        >
+          <ol className="header__list header__list_type_secondary">
             <li>
-              <a href="/state-of-parking/">State of Parking</a>
+              <a
+                className="header__link header__link_type_secondary"
+                href="/state-of-parking/"
+              >
+                State of Parking
+              </a>
             </li>
             <li>
-              <a href="/why-ai/">Why A.I?</a>
+              <a
+                className="header__link header__link_type_secondary"
+                href="/why-ai/"
+              >
+                Why A.I?
+              </a>
             </li>
             <li>
-              <a href="/product/">Product</a>
+              <a
+                className="header__link header__link_type_secondary"
+                href="/product/"
+              >
+                Product
+              </a>
             </li>
             <li>
-              <a href="/login/">Login</a>
+              <a
+                className="header__link header__link_type_secondary"
+                href="/login/"
+              >
+                Login
+              </a>
             </li>
           </ol>
         </nav>
@@ -56,17 +105,24 @@ export function Header() {
 
 export function Footer() {
   return (
-    <footer>
-      <ol className="list">
-        <li>
-          <a href="https://github.com/Anukaw100/parking-detection-ml-website">
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a href="/contact/">Contact Us</a>
-        </li>
-      </ol>
+    <footer className="footer">
+      <nav>
+        <ol className="footer__list">
+          <li>
+            <a
+              className="footer__link"
+              href="https://github.com/Anukaw100/parking-detection-ml-website"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a className="footer__link" href="/contact/">
+              Contact Us
+            </a>
+          </li>
+        </ol>
+      </nav>
     </footer>
   );
 }
