5d30c950dcfbb4a1bc375f2d18560ecd7cae2dc1
ss-raicangu <ssCm181126@outlook.com>
2021-06-11 17:38:04 +1200

    Rewrite flash message CSS
    - Remove curly brace for one-line anonymous function.
    - Rename and add class names for the flash message elements.
    - Create directory for flash message CSS.
    - Re-order the CSS elements by alphabetical ascending order.
    - Move the CSS stylesheet and fix its import.
    
    - Additional step for semantic correctness:
        - Convert closing cross in <p> tag to a button.
        - Add `onClick` attribute to the closing button.
        - Add background, border, box-sizing, and height CSS properties for
          closing button.
        - Edit margin and padding for closing button.

--- src/common/flash-message.jsx
+++ src/common/flash-message.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Bus from "./bus.js";
-import "./flash-message.css";
+import "./css/flash-message/alert.css";
 
 // https://medium.com/@jaouad_45834/building-a-flash-message-component-with-react-js-6288da386d53
 export default function FlashMessage() {
@@ -11,25 +11,21 @@ export default function FlashMessage() {
     Bus.addListener("flash", (newMessage) => {
       setVisibility(true);
       setMessage(newMessage);
-      setTimeout(() => {
-        setVisibility(false);
-      }, 4000);
+      // FIXME setTimeout(() => setVisibility(false), 4000);
     });
   }, []);
-
-  useEffect(() => {
-    if (document.querySelector(".close") !== null) {
-      document
-        .querySelector(".close")
-        .addEventListener("click", () => setVisibility(false));
-    }
-  });
 
   return (
     visibility && (
       <div className="alert">
-        <p>{message}</p>
-        <p className="close">X</p>
+        <p className="alert__message">{message}</p>
+        <button
+          className="alert__close-button"
+          type="button"
+          onClick={() => setVisibility(false)}
+        >
+          X
+        </button>
       </div>
     )
   );
