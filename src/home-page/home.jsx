4529d3ad31c6f8366e883d849dcf3d3f1ee9796b
ss-raicangu <ssCm181126@outlook.com>
2021-06-25 05:31:55 +1200

    Refactor home CSS
    - Create new component CSS files.
    - Fix the CSS imports.
    - Rename and add classes to fit BEM convention.
    - Remove the universal style of `box-sizing: border-box`. No noticeable
      change detected.
    - Combine "product-info", "ai-info", and "parking-info" into
      "section__info".
    - Copy CSS variables into component files, prefixing the component name.
    - Fix the `background-image` property of "ai-card" and "parking-card" to
      accurately reflect their appropriate images.
    - Replace `card-content > *:not(card-title)` selector with
      `card__body, card__button`.
    - Replace `card:hover card-content > *:not(card-title)` selector with
      `card:hover card__body, card:hover card__button`.
    - Fix the parking card's anchor `href`.
    - Complete the text-content in the card anchors.

--- src/home-page/home.jsx
+++ src/home-page/home.jsx
@@ -2,39 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Header, Footer } from "Common/common-sections.jsx";
 import "Common/css/universal/document.css";
-import "./home.css";
+import "./css/section.css";
+import "./css/card.css";
 
 function Info() {
   return (
     <main>
-      <section>
-        <div className="product-info">
+      <section className="section section_type_dark">
+        <div className="section__info">
           <h1>Our Future for Parking</h1>
           <p>Use A.I to assess a congested parking lot</p>
         </div>
-        <div className="card product-card">
-          <div className="card-content">
-            <h2 className="card-title">The Future of Parking</h2>
-            <p className="card-body">Try out a demo of our parking solution!</p>
-            <a href="/product" className="button">
-              Learn More
+        <div className="card card_topic_product">
+          <div className="card__content">
+            <h2 className="card__title">The Future of Parking</h2>
+            <p className="card__body">Try out a demo of our parking solution!</p>
+            <a href="/product" className="card__button">
+              Try out out Product!
             </a>
           </div>
         </div>
       </section>
-      <section className="light-section">
-        <div className="card ai-card">
-          <div className="card-content">
-            <h2 className="card-title">AI to the rescue</h2>
-            <p className="card-body">
+      <section className="section section_type_light">
+        <div className="card card_topic_ai">
+          <div className="card__content">
+            <h2 className="card__title">AI to the rescue</h2>
+            <p className="card__body">
               AI are helping us to solve real world problems.
             </p>
-            <a href="/why-ai" className="button">
-              Learn More
+            <a href="/why-ai" className="card__button">
+              Learn more about AI!
             </a>
           </div>
         </div>
-        <div className="ai-info">
+        <div className="section__info">
           <h1>Machine Learning is changing the world</h1>
           <p>
             Artifical Intelligence is getting smarter and better at helping us
@@ -42,8 +43,8 @@ function Info() {
           </p>
         </div>
       </section>
-      <section>
-        <div className="parking-info">
+      <section className="section section_type_dark">
+        <div className="section__info">
           <h1>Driving in circles</h1>
           <p>
             Parking is becoming more laborious every day as traffic increases.
@@ -51,14 +52,14 @@ function Info() {
             parking spot could be.
           </p>
         </div>
-        <div className="card parking-card">
-          <div className="card-content">
-            <h2 className="card-title">Parking is painful</h2>
-            <p className="card-body">
+        <div className="card card_topic_parking">
+          <div className="card__content">
+            <h2 className="card__title">Parking is painful</h2>
+            <p className="card__body">
               Cities are facing major problems with parking infrastructure.
             </p>
-            <a href="/product" className="button">
-              Learn More
+            <a href="/state-of-parking" className="card__button">
+              Learn more about Parking!
             </a>
           </div>
         </div>
